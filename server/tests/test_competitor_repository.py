12816fb96b14442c4eb67d18844e6054f23c9f7b
Patrik Turi <patrik.turi.0xff@gmail.com>
2019-10-23 22:01:52 +0200

    Add test case for Display Name preserved

--- server/tests/test_competitor_repository.py
+++ server/tests/test_competitor_repository.py
@@ -9,9 +9,8 @@ class CompetitorRepositoryTests(unittest.TestCase):
 
     def setUp(self):
         self.competitor1_id = 123
-        self.competitor2_id = 250
         self.competitor1 = Competitor(name='Competitor1', endomondo_id=self.competitor1_id, calories=1000)
-        self.competitor2 = Competitor(name='Competitor2', endomondo_id=self.competitor2_id, calories=1010)
+        self.competitor2 = Competitor(name='Competitor2', endomondo_id=250, calories=1010)
         self.competitors = [self.competitor1, self.competitor2]
 
         database.init_db()
@@ -40,6 +39,21 @@ class CompetitorRepositoryTests(unittest.TestCase):
         self.session.refresh(competitor)
         self.assertEqual(self.competitor1.name, competitor.name)
         self.assertEqual(self.competitor1.endomondo_id, competitor.endomondo_id)
+        self.assertEqual(self.competitor1.calories, competitor.calories)
+
+    def test_saveOrUpdate_existingDisplayNamePreserved(self):
+        # Name is parsed from Endomondo but DisplayName can be set in this webapp to override it
+        # so DisplayName always will be None when parsing data from Endomondo
+        display_name = 'Display Name'
+        orig_competitor = Competitor(name='Competitor1', display_name=display_name, endomondo_id=self.competitor1_id, calories=900)
+        self.session.add(orig_competitor)
+        self.session.commit()
+
+        self.assertEqual(self.competitor1.display_name, None)
+        self.repository.save_or_update(self.competitor1)
+
+        competitor = self.session.query(Competitor).filter_by(endomondo_id=self.competitor1_id).one()
+        self.assertEqual(display_name, competitor.display_name)
         self.assertEqual(self.competitor1.calories, competitor.calories)
 
     def test_saveOrUpdateAll_allCompetitorsCreated(self):
