a0e2c281d3fc276d15eab0b7cfbdacfb92d2da28
Patrik Turi <patrik.turi.0xff@gmail.com>
2020-02-24 23:32:25 +0100

    Create example template

--- endomondo_app/challanges/views.py
+++ endomondo_app/challanges/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
 
 
+# Last challange
 def index(request):
-    return render(request, 'index.html', {'name': 'You'})
+    context = {
+        'name': 'Last Challange Name',
+        'start': None,
+        'end': None,
+        'teams': [
+            {
+                'id': 3,
+                'name': 'TeamA',
+                'calories': 123,
+                'members': [
+                    {
+                        'name': 'Competitor 1',
+                        'calories': 50
+                    },
+                    {
+                        'name': 'Competitor 2',
+                        'calories': 73
+                    }
+                ]
+            },
+            {
+                'id': 5,
+                'name': 'TeamB',
+                'calories': 10,
+                'members': [
+                    {
+                        'name': 'Competitor 3',
+                        'calories': 10
+                    }
+                ]
+            }
+        ]
+    }
+    return render(request, 'index.html', context)
