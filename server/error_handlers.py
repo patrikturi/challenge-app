cd9024cbfcc7fa4b6df4cedeb3bf7d9367809c3b
Patrik Turi <patrik.turi.0xff@gmail.com>
2020-05-21 22:07:51 +0200

    Delete the old backend

--- server/error_handlers.py
+++
@@ -1,9 +0,0 @@
-from server import errors
-
-
-def handle_bad_request(e):
-    return {'success': False, 'message': f'Error: "{e}"'}
-
-
-def register(app):
-    app.register_error_handler(errors.UserError, handle_bad_request)
--- server/errors.py
+++
@@ -1,5 +0,0 @@
-
-
-class UserError(Exception):
-    """Invalid request/parameters, show error message to the user"""
-    pass
